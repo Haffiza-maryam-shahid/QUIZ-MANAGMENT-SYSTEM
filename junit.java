cd545c512c33d38f0f8be30d2139cea9847c3f22
Haffiza-maryam-shahid <87149580+Haffiza-maryam-shahid@users.noreply.github.com>
2022-06-21 22:27:36 -0700

    Add files via upload

---
+++ junit.java
@@ -0,0 +1,20 @@
+package project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class junit {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		junit obj =new junit();
+		int output = obj.onlinetest(
+	}
+
+}
